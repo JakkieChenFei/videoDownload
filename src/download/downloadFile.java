7242bbb7e270666c11c0f1206647b12d4d5d58e0
JakkieChenFei <744547287@qq.com>
2018-04-17 21:34:55 +0800

    init

---
+++ src/download/downloadFile.java
@@ -0,0 +1,9 @@
+package src.download;
+
+public class downloadFile {
+    private String filepath = null;
+    private int threadNum = 0;
+    public downloadFile (String filepath) {
+        this.filepath = filepath;
+    }
+}
